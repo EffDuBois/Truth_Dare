dd22028eba790145379dc329a2390b9348dd952e
EffDuBois <arnabkar2003@protonmail.com>
2023-08-15 17:32:28 +0530

    Wrote the basic choice of truth and dare

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Truth or Dare");
+        String Truth_or_dare_choice= in.next();
+        if (Objects.equals(Truth_or_dare_choice, "Truth"))
+                System.out.println("Truth question");
+        else if (Objects.equals(Truth_or_dare_choice, "Dare"))
+            System.out.println("Dare question");
     }
 }
